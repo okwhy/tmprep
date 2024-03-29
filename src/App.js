dbc10d0b92952df3e96aa57cd4aaca788d3c814f
okwhy <59335640+okwhy@users.noreply.github.com>
2024-03-29 10:44:38 +0400

    asdaa

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          AAAAAAA <code>src/App.js</code> and save to reload.
+          AAAAAAAkkkkkk <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
