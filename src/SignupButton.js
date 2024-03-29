e5257e40850119762de843f3f596bb44bc83ee0e
okwhy <59335640+okwhy@users.noreply.github.com>
2024-03-29 11:07:46 +0400

    as

---
+++ src/SignupButton.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SignupButton = () => {
+  const handleSignup = () => {
+    alert("Sign up successful!");
+  };
+
+  return (
+    <button onClick={handleSignup} className="signup-button">
+      Sign Up
+    </button>
+  );
+};
